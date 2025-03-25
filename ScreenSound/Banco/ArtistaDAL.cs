fb4f17d4c84e3f851abe10c1008a23dcc70d5e00
MateusGovea <matrgovea@gmail.com>
2025-03-25 13:27:48 -0300

    Adicionar arquivos de projeto.

---
+++ ScreenSound/Banco/ArtistaDAL.cs
@@ -0,0 +1,53 @@
+﻿using ScreenSound.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenSound.Banco
+{
+    internal class ArtistaDAL
+    {
+
+        private readonly ScreenSoundContext context;
+
+        public ArtistaDAL(ScreenSoundContext context)
+        {
+            this.context = context;
+        }
+
+        public IEnumerable<Artista> Listar()      // Método de SELECT
+        {
+            return context.Artistas.ToList();     // Retornando a lista de artistas
+        }
+
+        public void Adicionar (Artista artista)   // Método de INSERT
+        {
+            // Conexão
+            // Não é necessário, pois instanciamos a classe já possuindo um context
+
+            // Adicionando o artista
+            context.Artistas.Add(artista);
+            context.SaveChanges();
+        }
+
+        public void Atualizar(Artista artista) // Método de UPDATE
+        {
+            context.Artistas.Update(artista);
+            context.SaveChanges();
+        }
+
+        public void Deletar(Artista artista) // Método de DELETE
+        {
+            context.Artistas.Remove(artista);
+            context.SaveChanges();
+        }
+
+        public Artista? RecuperarPeloNome(string nome)
+        {
+            return context.Artistas.FirstOrDefault(a => a.Equals(nome));
+        }
+    }
+}
---
+++ ScreenSound/Banco/ScreenSoundContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ScreenSound.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenSound.Banco
+{
+    internal class ScreenSoundContext : DbContext
+    {
+        // Linkando o objeto(1) à tabela do banco(2)
+        public DbSet<Artista> Artistas { get; set; }
+
+
+        private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=ScreenSound;Integrated Security=True;" +
+            "Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        // Conexão com o banco
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
